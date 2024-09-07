381499227b814e8ac26df43fffc64f6ded6986e1
Shubham <mynameshubham23@gmail.com>
2024-09-07 15:15:59 +0530

    text extraction from pdf"

--- simple_rag.py
+++ simple_rag.py
@@ -38,4 +38,19 @@ def text_formatter(text: str) -> str:
 def open_and_read_pdf(pdf_path: str) -> list[dict]:
     doc.fitz.open(pdf_path)
     pages_and_texts = []
-    
+    for page_number, page in tqdm(enumerate(doc)):
+        text = page.get_text()
+        text = text_formatter(text=text)
+        pages_and_texts.append({
+            "page_number": page_number - 41,
+            "page_char_count": len(text),
+            "page_word_count": len(text.split(" ")),
+            "page_sentence_count": len(text.split(". ")),
+            "page_token_count": len(text) / 4,
+            "text": text
+        })
+    return pages_and_texts
+
+pages_and_texts = open_and_read_pdf(pdf_path=pdf_path)
+
+print(pages_and_texts)
